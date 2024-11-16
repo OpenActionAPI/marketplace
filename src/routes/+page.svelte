b854230c63416988d16fdb8739f35b4f8554e42b
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2024-11-16 17:01:13 +0000

    Styling, organisation, and formatting improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,49 +2,32 @@
 	import { onMount } from "svelte";
 	import Plugin from "../components/Plugin.svelte";
 
-	let loading = true;
 	let data: { [id: string]: { name: string; repository: string; author: string } };
 	let searchString = "";
 
 	onMount(async () => {
-		try {
-			const response = await fetch(
-				"https://ninjadev64.github.io/openaction-plugins/catalogue.json",
-			);
-
-			data = await response.json();
-		} catch (err: any) {
-			console.error(err.message);
-		} finally {
-			loading = false;
-		}
+		const response = await fetch("https://ninjadev64.github.io/openaction-plugins/catalogue.json");
+		data = await response.json();
 	});
 </script>
 
-<svelte:head>
-	<title>OpenAction Marketplace</title>
-	<link rel="icon" href="/favicon.png" />
-</svelte:head>
-
 <div class="p-3">
-	<div class="p-2 px-8 flex flex-col md:flex-row items-center justify-between">
-		<h1 class="text-2xl md:text-3xl text-white font-bold pb-1.5">OpenAction Marketplace</h1>
+	<div class="py-2 px-8 flex flex-col md:flex-row items-center justify-between">
+		<h1 class="pb-1.5 font-bold text-2xl md:text-3xl text-white">OpenAction Marketplace</h1>
 		<input
 			bind:value={searchString}
 			placeholder="Search for plugins"
-			class="mt-8 md:mt-4 mr-4 ml-4 md:mr-0 p-4 h-12 w-full md:w-72 lg:w-96 text-center rounded-xl text-white bg-[#31363F]"
+			class="mt-8 md:mt-4 mx-4 md:mr-0 p-4 w-full md:w-72 lg:w-[32rem] h-12 text-center text-white bg-[#31363F] rounded-xl"
 		/>
 	</div>
 
-	{#if !loading}
-		<grid>
-			<div class="m-auto p-8 gap-4 gap-y-12 grid-container grid">
-				{#each Object.entries(data) as [key, plugin]}
-					{#if plugin.name.toLowerCase().includes(searchString.toLowerCase())}
-						<Plugin {plugin} id={key}></Plugin>
-					{/if}
-				{/each}
-			</div>
-		</grid>
+	{#if data}
+		<div class="p-8 gap-x-4 gap-y-12 grid-container grid">
+			{#each Object.entries(data) as [key, plugin]}
+				{#if plugin.name.toLowerCase().includes(searchString.toLowerCase())}
+					<Plugin {plugin} id={key}></Plugin>
+				{/if}
+			{/each}
+		</div>
 	{/if}
 </div>
--- src/routes/plugin/[id]/+page.svelte
+++ src/routes/plugin/[id]/+page.svelte
@@ -6,25 +6,18 @@
 	import ArrowSquareOut from "phosphor-svelte/lib/ArrowSquareOut";
 	import DownloadCount from "../../../components/DownloadCount.svelte";
 
-	let pluginDetails: { repository: string; name: string; author: string; download_url: string };
+	let pluginDetails: { repository: string; name: string; author: string; download_url: string | undefined };
+	let readme = "";
+	let download_count = 0;
 
-	let readme: string | undefined;
-	let download_count: number;
+	async function getTotalDownloadCount(repo: string): Promise<number> {
+		const response = await fetch("https://api.github.com/repos/" + repo + "/releases");
+		const releases = await response.json();
 
-	let response: Response;
-	let loading = true;
-
-	async function getDownloadAction(response: any, r: string) {
-		if (pluginDetails.download_url) return pluginDetails.download_url;
-		return pluginDetails.repository + "/releases/latest";
-	}
-
-	function getTotalDownloadCount(response: any) {
 		let totalCount = 0;
-
-		for (let release in response) {
-			for (let asset in response[release].assets) {
-				totalCount += response[release].assets[asset].download_count;
+		for (let release of releases) {
+			for (let asset of release.assets) {
+				totalCount += asset.download_count;
 			}
 		}
 
@@ -32,132 +25,110 @@
 	}
 
 	async function getReadme(repo: string): Promise<string> {
-		let readme = await fetch(
-			"https://raw.githubusercontent.com/" +
-				repo +
-				"/" +
-				"master" +
-				"/README.md",
-		);
-		return await readme.text();
-	}
-
-	function trimRepo(r: string) {
-		return r.split("/")[3] +
-			"/" +
-			r.split("/")[4];
+		let response = await fetch("https://raw.githubusercontent.com/" + repo + "/master/README.md");
+		return await response.text();
 	}
 
 	onMount(async () => {
-		try {
-			const response = await fetch("https://ninjadev64.github.io/openaction-plugins/catalogue.json");
-			if (!response.ok) throw new Error("Network response was not ok");
+		const response = await fetch("https://ninjadev64.github.io/openaction-plugins/catalogue.json");
+		if (!response.ok) return;
 
-			let data = await response.json();
-			pluginDetails = data[$page.params.id];
-
-			if (!pluginDetails) return;
-		} catch (err: any) {
-			console.error(err.message);
-		} finally {
-			let repo = trimRepo(pluginDetails.repository);
-			response = await fetch("https://api.github.com/repos/" + repo + "/releases");
-			response = await response.json();
-			readme = await getReadme(repo);
-			download_count = getTotalDownloadCount(response);
-
-			loading = false;
+		const data = await response.json();
+		pluginDetails = data[$page.params.id];
+		if (!pluginDetails) {
+			// @ts-expect-error
+			pluginDetails = -1;
+			return;
 		}
+
+		const repo = pluginDetails.repository.split("/")[3] + "/" + pluginDetails.repository.split("/")[4];
+		readme = await getReadme(repo);
+		download_count = await getTotalDownloadCount(repo);
 	});
 
 	function openDeepLink() {
-		// @ts-ignore
-		document.getElementById("deeplink-iframe").src ="opendeck://installPlugin/" + $page.params.id;
-    }
+		let iframe = document.getElementById("deeplink-iframe") as HTMLIFrameElement;
+		iframe.src = "opendeck://installPlugin/" + $page.params.id;
+	}
+
+	function getDownloadAction(): string {
+		if (pluginDetails.download_url) return pluginDetails.download_url;
+		return pluginDetails.repository + "/releases/latest";
+	}
 </script>
 
 <svelte:head>
-	{#if !loading && pluginDetails}
+	{#if pluginDetails}
 		<title>{pluginDetails.name} - OpenAction Marketplace</title>
-		<link
-			rel="icon"
-			type="image/x-icon"
-			href={"https://ninjadev64.github.io/openaction-plugins/icons/" +
-			$page.params.id +
-			".png"}
-		/>
 	{/if}
+	<link
+		rel="icon"
+		type="image/x-icon"
+		href={"https://ninjadev64.github.io/openaction-plugins/icons/" + $page.params.id + ".png"}
+	/>
 </svelte:head>
 
-{#if !loading && pluginDetails}
-	<div class="ml-[5%] mt-[2.5%] p-10 w-[90%] flex flex-col rounded-3xl bg-[#31363F]">
-		<div class="flex flex-col md:flex-row items-center justify-between">
-			<div class="flex">
+{#if pluginDetails}
+	<div class="mx-[2.5%] my-[2.5%] p-10 w-[95vw] h-[90vh] flex flex-col bg-[#31363F] rounded-3xl">
+		<div class="flex flex-col md:flex-row items-center md:items-start justify-between">
+			<div class="flex flex-row items-center md:items-start">
 				<img
-					src={"https://ninjadev64.github.io/openaction-plugins/icons/" +
-					$page.params.id +
-					".png"}
-					alt="Plugin Icon"
-					class="size-[70px] md:size-[200px] md:rounded-3xl rounded-2xl"
+					src={"https://ninjadev64.github.io/openaction-plugins/icons/" + $page.params.id + ".png"}
+					alt={pluginDetails.name}
+					class="size-16 md:size-48 rounded-2xl md:rounded-3xl"
 				/>
-				<div class="ml-8 mt-0 flex flex-col">
-					<div class="text-3xl text-white">
-						{pluginDetails.name}
-					</div>
-					<p class="mt-2 flex text-lg text-slate-400">
-						<span class="mr-1 text-lg"> by </span>
+				<div class="ml-8 flex flex-col">
+					<div class="text-3xl text-white">{pluginDetails.name}</div>
+					<div class="mt-2 flex items-center text-lg text-slate-400">
+						<span class="mr-2">by</span>
 						<img
 							src={"https://avatars.githubusercontent.com/" +
 							pluginDetails.repository.split("/")[3]}
 							alt="Avatar URL"
-							class="size-[30px] rounded-3xl"
+							class="mr-1.5 size-7 rounded-full"
 						/>
-						<span class="mr-1 text-lg"></span>
 						<a
-							href={"https://github.com/" +
-							pluginDetails.repository.split("/")[3]}
+							href={"https://github.com/" + pluginDetails.repository.split("/")[3]}
 							class="underline"
-						>{pluginDetails.repository.split("/")[3]}</a>
-					</p>
+						>
+							{pluginDetails.repository.split("/")[3]}
+						</a>
+					</div>
 				</div>
 			</div>
 
-			<div class="md:mb-40 mb-10 mt-5 md:mt-0 flex">
-				<iframe title="deeplinkiframe" id="deeplink-iframe" style="display: none;"></iframe>
-				
+			<div class="my-8 md:mt-0 flex flex-row items-center">
+				<iframe title="deeplink-iframe" id="deeplink-iframe" class="hidden"></iframe>
+
 				<button
 					on:click={() => openDeepLink()}
-					class="pb-1.5 pl-8 pr-8 pt-1.5 md:pb-3 md:pt-3 cursor-pointer text-lg text-slate-100 active:translate-y-0.5 bg-blue-600 rounded-l-lg"
+					class="px-8 py-1.5 md:py-3 active:translate-y-0.5 text-lg text-slate-100 bg-blue-600 rounded-l-lg"
 				>
 					Install
 				</button>
 
 				<button
-					on:click={async () => {
-						window.open(await getDownloadAction(response, trimRepo(pluginDetails.repository)));
-					}}
-					class="ml-1 p-3 cursor-pointer text-lg text-slate-100 bg-blue-600 rounded-r-lg"
+					on:click={async () => window.open(getDownloadAction())}
+					class="ml-1 p-2 md:p-3.5 active:translate-y-0.5 text-lg text-slate-100 bg-blue-600 rounded-r-lg"
 				>
 					<ArrowSquareOut size={24} />
 				</button>
 
-				<div class="ml-8 mt-2">
+				<div class="ml-8">
 					<DownloadCount count={download_count} />
 				</div>
 			</div>
 		</div>
 
-		<div class="mt-0 md:mt-5 p-6 h-96 overflow-scroll rounded-xl border-4 border-slate-600">
+		<div class="md:mt-8 p-6 h-full overflow-scroll border-4 border-slate-600 rounded-xl">
 			<p class="readme text-slate-300">
-				{@html marked.parse(readme!)}
+				{@html marked.parse(readme)}
 			</p>
 		</div>
 	</div>
-{/if}
-
-{#if !pluginDetails}
+{:else if pluginDetails == -1}
 	<div class="m-auto p-4 items-center">
-		<h1 class="font-bold text-3xl text-white">404 Plugin Not found</h1>
-		<h1 class="font-semibold text-xl text-white">Please check the plugin ID was spelled correctly</h1>
+		<h1 class="font-bold text-3xl text-white">404 Plugin not found</h1>
+		<h1 class="font-semibold text-xl text-white">Please check that the plugin ID was entered correctly</h1>
 	</div>
 {/if}
